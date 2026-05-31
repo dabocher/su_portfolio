547ad17a0fd015dabe3ddccf4e7b17c50595ae20
David Botello <dabocher@gmail.com>
2026-05-31 20:49:12 +0200

    Add favicon and logo on header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
 import NavBar from "./NavBar";
 
 const Header = () => {
   return (
     <header className="w-full  flex items-center justify-center">
+      <div className="">
+        <Image
+          src="/images/home/susulogo.webp"
+          alt="Logo Susana Subirana"
+          width={32}
+          height={32}
+          className="rounded-lg object-cover"
+        />
+      </div>
       <NavBar />
     </header>
   );
