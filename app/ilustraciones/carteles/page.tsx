2f7d12d01904c4bf64ee186e66fa49620e25aa2b
David Botello <dabocher@gmail.com>
2026-06-05 10:04:28 +0200

    Delete padding on CartelesPage

--- app/ilustraciones/carteles/page.tsx
+++ app/ilustraciones/carteles/page.tsx
@@ -2,7 +2,7 @@ import { BannerCarousel } from "@/app/components/BannerCarousel";
 
 const CartelesPage = () => {
   return (
-    <main className="flex flex-col items-center justify-center p-4  font-sans h-full">
+    <main className="flex flex-col items-center justify-center font-sans h-full text-zinc-800">
       <BannerCarousel />
     </main>
   );
