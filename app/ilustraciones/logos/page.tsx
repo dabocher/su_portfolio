4265867d88a5e2aa28d8373669bf96b0c9ad2ba4
David Botello <dabocher@gmail.com>
2026-06-07 19:33:26 +0200

    Change grid columns size on diferent devices

--- app/ilustraciones/logos/page.tsx
+++ app/ilustraciones/logos/page.tsx
@@ -10,7 +10,7 @@ const LogosPage = () => {
   return (
     <main className="flex flex-col h-full w-full  font-sans text-zinc-800">
       <div className="w-full  bg-[#f5efe6]  p-4 rounded-lg">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 w-full place-items-center gap-4 ">
+        <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 w-full place-items-center gap-4 ">
           {images.map((image) => (
             <CardLogo key={image.id} src={image.src} alt={image.alt} />
           ))}
