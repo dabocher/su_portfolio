95472ebb056ba6c579cc2edabe5c28f9c0a7a675
David Botello <dabocher@gmail.com>
2026-06-07 11:50:47 +0200

    Change grid collage for a simple collage image on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CardHome from "./components/CardHome";
 
 const images = Array.from({ length: 9 }, (_, i) => ({
@@ -8,15 +9,18 @@ const images = Array.from({ length: 9 }, (_, i) => ({
 
 const HomePage = () => {
   return (
-    <main className="flex flex-col items-center justify-center font-sans gap-4 md:gap-8 md:pt-8 text-zinc-800 ">
+    <main className="flex flex-col items-center justify-center p-4 font-sans gap-4 md:gap-8 md:pt-8 text-zinc-800 ">
       <h1 className="text-[48px] md:text-[64px] font-semibold leading-10 tracking-tight text-center">
         Susana Subirana <span className="text-red-600">Portfolio</span>
       </h1>
       <div className="w-full  bg-[#f5efe6]  p-4 rounded-lg">
-        <div className="grid grid-cols-3 gap-4 ">
-          {images.map((image) => (
-            <CardHome key={image.id} src={image.src} alt={image.alt} />
-          ))}
+        <div className="h-full w-full">
+          <Image
+            src="/images/home/collagehome.webp"
+            alt="Collage Home"
+            width={500}
+            height={500}
+          />
         </div>
       </div>
     </main>
