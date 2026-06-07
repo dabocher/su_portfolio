742305332f600e33f904305aa84bb2c9ed672af3
David Botello <dabocher@gmail.com>
2026-06-07 20:30:25 +0200

    Add media icons on footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <footer className="w-full h-16 flex flex-col items-center justify-center text-sm text-zinc-600">
+      <div className="flex gap-4">
+        <Link
+          href="https://www.instagram.com/susanasubiranalopez/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faInstagram} className="w-5 h-5" />
+        </Link>
+        <Link href="https://www.facebook.com/susanasubi" target="_blank">
+          <FontAwesomeIcon icon={faFacebook} className="w-5 h-5" />
+        </Link>
+        <Link href="https://x.com/susiyole" target="_blank">
+          <FontAwesomeIcon icon={faXTwitter} className="w-5 h-5" />
+        </Link>
+        <Link href="mailto:susanasusana363@gmail.com" target="_blank">
+          <FontAwesomeIcon icon={faEnvelope} className="w-5 h-5" />
+        </Link>
+      </div>
+      <p>© 2026 Susana Subirana. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono, Amatic_SC, Yanone_Kaffeesatz } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { Footer } from "./components/Footer";
 
 const amaticSC = Amatic_SC({
   variable: "--font-amatic-sc",
@@ -38,6 +39,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col items-center gap-12 sd:gap-8   bg-[#f5efe6aa] text-red-600">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
