7db7574998925746effe1070403e0c30a4ec9d01
David Botello <dabocher@gmail.com>
2026-06-11 07:19:38 +0200

    Delete Contacto, Animaciones and Viñetas pages

--- app/ilustraciones/vinetas/page.tsx
+++
@@ -1,9 +0,0 @@
-const VinetasPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-full">
-      Aquí van las viñetas
-    </div>
-  );
-};
-
-export default VinetasPage;
