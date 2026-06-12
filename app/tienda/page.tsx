92d15c1ec4f91a49369f06d03048fc371bf9d771
David Botello <dabocher@gmail.com>
2026-06-12 13:00:40 +0200

    Add grid col property and make responsive

--- app/tienda/page.tsx
+++ app/tienda/page.tsx
@@ -3,7 +3,7 @@ import { obras } from "../data";
 
 const TiendaPage = () => {
   return (
-    <main className=" rounded-2xl flex flex-col gap-4 font-yanone mx-6 text-zinc-800">
+    <main className=" rounded-2xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 sm:gap-4 lg: gap-4 font-yanone mx-6 text-zinc-800">
       {obras.map((obra) => (
         <CardShop
           src={obra.image}
