0b197edfbe62f47fb59851e249ae4d8c952ea212
David Botello <dabocher@gmail.com>
2026-06-14 12:39:34 +0200

    Delete properties for print

--- app/components/CardShop.tsx
+++ app/components/CardShop.tsx
@@ -18,30 +18,25 @@ export const CardShop = ({
   resolution,
 }: CardShopProps) => {
   return (
-    <div className="bg-[#f5efe6] text-zinc-800 p-4 rounded-lg flex flex-col gap-4  ">
+    <div className="bg-[#f5efe6] min-h-full text-zinc-800 p-4 rounded-lg flex flex-col gap-4  ">
       <div className="relative w-full aspect-video">
-        <Image src={src} alt={alt} fill className="rounded-lg object-cover" />
+        <Image
+          src={src}
+          alt={alt}
+          fill
+          sizes="500px"
+          className="rounded-lg object-cover"
+        />
       </div>
-      <h2 className="font-sans text-2xl">{title}</h2>
-      <h3>{description}</h3>
+      <h2 className="font-sans text-2xl font-semibold">{title}</h2>
+      <div className="flex flex-col flex-1 gap-6 justify-between ">
+        <h3>{description}</h3>
 
-      <div className="flex justify-between">
-        <div>
-          <p>Calidad papel: </p>
-          <p>300g/m2</p>
-        </div>
-        <div>
-          <p>Resolución: </p> <p>{resolution}</p>
-        </div>
-        <div>
-          <p>Tamaños disponibles: </p> <p>3600 x 1800 cm</p>
-        </div>
-      </div>
+        <div className="flex justify-between">
+          <p>Precio: 36 euros</p>
 
-      <div className="flex justify-between">
-        <p>Precio: 36 euros</p>
-
-        <button className="rounded-lg px-4 py-1 bg-green-300">Comprar</button>
+          <button className="rounded-lg px-4 py-1 bg-green-300">Comprar</button>
+        </div>
       </div>
     </div>
   );
--- app/tienda/page.tsx
+++ app/tienda/page.tsx
@@ -3,17 +3,28 @@ import { obras } from "../data";
 
 const TiendaPage = () => {
   return (
-    <main className=" rounded-2xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 sm:gap-4 lg: gap-4 font-yanone mx-6 text-zinc-800">
-      {obras.map((obra) => (
-        <CardShop
-          src={obra.image}
-          alt={""}
-          title={obra.title}
-          description={obra.description}
-          resolution={obra.resolution}
-          key={obra.id}
-        />
-      ))}
+    <main className="flex flex-col gap-4  justify-center py-4">
+      <div className="mx-6 flex flex-col gap-2 text-2xl p-4 rounded-lg  bg-[#f5efe6] text-zinc-800">
+        <h3 className="text-3xl font-bold ">"Flamencas Ilustradas"</h3>
+        <p className="text-xl text-justify font-yanone">
+          Proyecto colaborativo entre dos artistas, Arte sobre Arte. Colección
+          de mujeres flamencas en blanco y negro con fotografías de Isa de Calle
+          e ilustración de Susana Subirana. Presentada dentro del Jerez OFF
+          Festival de 2023.
+        </p>
+      </div>
+      <section className=" rounded-2xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 sm:gap-4 lg: gap-4 font-yanone mx-6 text-zinc-800">
+        {obras.map((obra) => (
+          <CardShop
+            src={obra.image}
+            alt={""}
+            title={obra.title}
+            description={obra.description}
+            resolution={obra.resolution}
+            key={obra.id}
+          />
+        ))}
+      </section>
     </main>
   );
 };
